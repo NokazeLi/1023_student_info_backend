964a7ac6a404c0b4dd0d2bfe0df0d04bdbe7c389
wanjj0325 <wanjunjie3@hotmail.com>
2025-01-23 12:35:15 +0800

    请求

--- src/main/java/org/jdsystem/studentinfobackend/controller/StudentController.java
+++ src/main/java/org/jdsystem/studentinfobackend/controller/StudentController.java
@@ -1,4 +1,17 @@
 package org.jdsystem.studentinfobackend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.jdsystem.studentinfobackend.sevice.IStudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
 public class StudentController {
+    private final IStudentService studentService;
+
 }
--- src/main/java/org/jdsystem/studentinfobackend/sevice/StudentService.java
+++ src/main/java/org/jdsystem/studentinfobackend/sevice/StudentService.java
@@ -1,4 +1,20 @@
 package org.jdsystem.studentinfobackend.sevice;
 
-public class StudentService {
+import lombok.RequiredArgsConstructor;
+import org.jdsystem.studentinfobackend.model.Student;
+import org.jdsystem.studentinfobackend.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService implements IStudentService{
+    private final StudentRepository studentRepository;
+
+    @Override
+    public void  deleteStudent(Long id) {
+        if (!studentRepository.existsById(id)) {
+            throw new StudentNotFoundException("Sorry.student not found");
+        }
+    }
+
 }
