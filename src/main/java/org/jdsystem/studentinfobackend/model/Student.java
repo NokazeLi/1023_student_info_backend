c5fd62c19e74502c5a329cd474df19e51d765adc
RYeahub <hyr1010715@gmail.com>
2025-01-24 13:59:19 +0900

    get student by ID

--- src/main/java/org/jdsystem/studentinfobackend/model/Student.java
+++ src/main/java/org/jdsystem/studentinfobackend/model/Student.java
@@ -4,16 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.NaturalId;
+import org.springframework.stereotype.Service;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-//1231
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/org/jdsystem/studentinfobackend/service/IStudentService.java
@@ -0,0 +1,15 @@
+package org.jdsystem.studentinfobackend.service;
+
+import org.jdsystem.studentinfobackend.model.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+//    List<Student> getStudents();
+//    Student addStudent(Student student);
+//    Student updateStudent(Student student, Long id);
+    Student getStudentById(Long id);
+//    void deleteStudent(Long id);
+
+
+}
